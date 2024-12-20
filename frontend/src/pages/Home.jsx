0cba1c504bb942436d209b9797257fbcd9ac9551
Aman Agarwal <aarpit688@gmail.com>
2024-12-20 16:19:26 +0530

    Created Create Post Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Loader, Card, FormField } from "../components";
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
-    return data.map((post) => <Card key={post.id} {...post} />);
+    return data.map((post) => <Card key={post._id} {...post} />);
   }
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
@@ -44,7 +44,13 @@ const Home = () => {
                   <span className="text-[#222328]">{searchText}</span>
                 </h2>
               )}
-              <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3"></div>
+              <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+                {searchText ? (
+                  <RenderCards data={[]} title="No results found" />
+                ) : (
+                  <RenderCards data={[]} title="No posts found" />
+                )}
+              </div>
             </>
           )}
         </div>
